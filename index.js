09be99cfd17302400d45a0ccf447c96b80fd2066
Jairo Hernández Hernández <hdezhdezjairojosue@gmail.com>
2022-09-05 18:58:15 -0500

    Cambio de forma de importación de Lit con Bundles

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {LitElement, html, css} from './node_modules/lit-element/lit-element.js'
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 
 import "./components/lit-header.js";
 import "./components/lit-search.js";
